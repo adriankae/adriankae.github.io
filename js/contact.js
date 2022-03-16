6e63fa4c5776f3f53d07a541bb782d4d4c5df5c1
Adrian Käsdorf <15908481+adriankae@users.noreply.github.com>
2022-03-16 18:34:18 +0100

    added js for contact to send message to user

---
+++ js/contact.js
@@ -0,0 +1,27 @@
+function ajaxpost () {
+  alert("Thank you for your message!")
+
+  // // https://code-boxx.com/display-message-after-submitt-html-form/
+  //
+  // // (A) GET FORM DATA
+  // var form = document.getElementById("contact-form");
+  // var data = new FormData(form);
+  //
+  // // (B) AJAX REQUEST
+  // // (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
+  // fetch("1c-server.html", { method:"POST", body:data })
+  // .then(res=>res.text())
+  //
+  // // (B2) SHOW MESSAGE ON SERVER RESPONSE
+  // .then((response) => {
+  //   console.log(response);
+  //   if (response == "OK") { alert("SUCCESSFUL!"); }
+  //   else { alert("FAILURE!"); }
+  // })
+  //
+  // // (B3) OPTIONAL - HANDLE FETCH ERROR
+  // .catch((err) => { console.error(err); });
+
+  // (C) PREVENT FORM SUBMIT
+  // return false;
+}
