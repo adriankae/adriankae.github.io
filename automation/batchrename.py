c1e829332d00ac464b88f01de96404b3d3df63bf
Adrian Käsdorf <15908481+adriankae@users.noreply.github.com>
2022-04-13 08:08:27 +0200

    minor structural changes

--- automation/batchrename.py
+++ automation/batchrename.py
@@ -1,15 +1,14 @@
+#!/usr/bin/env python3
+
 import os
 join = os.path.join
 
 path = '/Users/adrian/Documents/github_pages/docs/assets/images'
 directory = os.listdir(path)
 
-print('start loop')
 for item in directory:
-    print('item is: ' + item)
     if os.path.isdir(path + '/' + item):
-        print('### found folder ###')
-        print('folder is: ' + item)
         for file in os.listdir(path + '/' + item):
-            print('file is ' + file)
-            os.rename(os.path.join(path + '/' + item, file), os.path.join(path + '/' + item, file.replace(' - ', '')))
+            if " " in file:
+                print('file is ' + file)
+                os.rename(os.path.join(path + '/' + item, file), os.path.join(path + '/' + item, file.replace(' - ', '')))
