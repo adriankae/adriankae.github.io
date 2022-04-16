e3d5d14a98986f60e7920c08c504e6ab8201dcb4
Adrian Käsdorf <15908481+adriankae@users.noreply.github.com>
2022-04-16 15:08:05 +0200

    reduced files sizes

--- automation/resize_large_images.py
+++ automation/resize_large_images.py
@@ -7,7 +7,7 @@ from PIL import Image
 join = os.path.join
 
 path = '/Users/adrian/Documents/github_pages/docs/assets/images/'
-
+max_file_size = 5000000
 try:
     directory = os.listdir(path)
 except FileNotFoundError:
@@ -18,7 +18,8 @@ for item in directory:
     if os.path.isdir(path + '/' + item):
         for file in os.listdir(path + '/' + item):
             filename, file_extension = os.path.splitext(file)
-            if os.stat(path + '/' + item + '/' + file).st_size > 50000000 and file_extension == '.jpeg':
+            # if files are bigger as 5 MB (max_file_size) then resize to 30%
+            if os.stat(path + '/' + item + '/' + file).st_size > max_file_size and file_extension == '.jpeg':
                 new_image = Image.open(path + '/' + item + '/' + file)
                 new_image = new_image.resize(tuple(round(0.3*x) for x in new_image.size))
                 new_image.save(path + '/' + item + '/' + file)
