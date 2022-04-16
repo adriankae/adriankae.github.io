ecf17276d6ef83da9ab3547cf12f6ee891ef1115
Adrian Käsdorf <15908481+adriankae@users.noreply.github.com>
2022-04-16 14:14:44 +0200

    resized large pictures

---
+++ automation/resize_large_images.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from PIL import Image
+
+join = os.path.join
+
+path = '/Users/adrian/Documents/github_pages/docs/assets/images/'
+
+try:
+    directory = os.listdir(path)
+except FileNotFoundError:
+    print('The path \'/Users/adrian/Documents/github_pages/docs/assets/images\' used in the script cannot be found.')
+    sys.exit()
+
+for item in directory:
+    if os.path.isdir(path + '/' + item):
+        for file in os.listdir(path + '/' + item):
+            filename, file_extension = os.path.splitext(file)
+            if os.stat(path + '/' + item + '/' + file).st_size > 50000000 and file_extension == '.jpeg':
+                new_image = Image.open(path + '/' + item + '/' + file)
+                new_image = new_image.resize(tuple(round(0.3*x) for x in new_image.size))
+                new_image.save(path + '/' + item + '/' + file)
+                print('' + path + '/' + item + '/' + file + ' has been resized.')
