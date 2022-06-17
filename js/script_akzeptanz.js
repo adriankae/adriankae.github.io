84f8c5f0fa53d6ec4efe5fb81672e40dba2034f7
Adrian Käsdorf <15908481+adriankae@users.noreply.github.com>
2022-06-17 15:48:51 +0200

    fixed buttons

--- js/script_akzeptanz.js
+++ js/script_akzeptanz.js
@@ -3,7 +3,7 @@
 let step = document.getElementsByClassName('step');
 let prevBtn = document.getElementById('prev-btn');
 let nextBtn = document.getElementById('next-btn');
-// let dnldBtn = document.getElementById('dnld-btn');
+let dnldBtn = document.getElementById('dnld-btn');
 let submitBtn = document.getElementById('submit-btn');
 let form = document.getElementsByTagName('form')[0];
 let preloader = document.getElementById('preloader-wrapper');
@@ -18,7 +18,7 @@ let stepCount = 20;
 step[current_step].classList.add('d-block');
 if (current_step == 0) {
     prevBtn.classList.add('d-none');
-    // dnldBtn.classList.add('d-none');
+    dnldBtn.classList.add('d-none');
     submitBtn.classList.add('d-none');
     nextBtn.classList.add('d-inline-block');
 }
@@ -33,19 +33,20 @@ nextBtn.addEventListener('click', () => {
     if ((current_step > 0) && (current_step <= stepCount)) {
         prevBtn.classList.remove('d-none');
         prevBtn.classList.add('d-inline-block');
+        dnldBtn.classList.add('d-none');
         console.log(step[current_step]);
         console.log(current_step);
-        step[current_step].classList.remove('d-none');
-        step[current_step].classList.add('d-block');
         step[previous_step].classList.remove('d-block');
         step[previous_step].classList.add('d-none');
+
+        step[current_step].classList.remove('d-none');
+        step[current_step].classList.add('d-block');
+
         if (current_step == stepCount) {
             submitBtn.classList.remove('d-none');
             submitBtn.classList.add('d-inline-block');
             nextBtn.classList.remove('d-inline-block');
             nextBtn.classList.add('d-none');
-            dnldBtn.classList.remove('d-none');
-            dnldBtn.classList.add('d-inline-block');
         }
     } else {
         if (current_step > stepCount) {
@@ -64,11 +65,14 @@ prevBtn.addEventListener('click', () => {
         let previous_step = current_step + 1;
         prevBtn.classList.add('d-none');
         prevBtn.classList.add('d-inline-block');
+        dnldBtn.classList.add('d-none');
         step[current_step].classList.remove('d-none');
         step[current_step].classList.add('d-block')
         step[previous_step].classList.remove('d-block');
         step[previous_step].classList.add('d-none');
         if (current_step < stepCount) {
+            // dnld.classList.remove('d-inline-block');
+            dnldBtn.classList.add('d-none');
             submitBtn.classList.remove('d-inline-block');
             submitBtn.classList.add('d-none');
             nextBtn.classList.remove('d-none');
@@ -103,13 +107,15 @@ submitBtn.addEventListener('click', () => {
             submitBtn.classList.add('d-none');
             succcessDiv.classList.remove('d-none');
             succcessDiv.classList.add('d-block');
+            dnldBtn.classList.remove('d-none');
+            dnldBtn.classList.add('d-inline-block');
         })
 
 });
 
-// dnldBtn.addEventListener('click', () => {
-//     createPdf();
-// });
+dnldBtn.addEventListener('click', () => {
+    createPdf();
+});
 
 function createPdf() {
   /*
@@ -170,13 +176,13 @@ function createPdf() {
       title: {
         fontSize: 22,
         bold: true,
-        color: '#E85A4F',
+        color: '#022C6E',
         font : 'Roboto'
       },
       header: {
         fontSize: 16,
         bold: true,
-        color: '#E98074'
+        color: '#2832D2'
       },
       footer: {
         fontSize: 10,
